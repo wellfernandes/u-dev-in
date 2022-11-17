90034a796672c5da6ef56469b75fa361434fa567
Welliton Fernandes Leal <wellfernandes@me.com>
2022-11-17 10:26:00 -0300

    creating a router and returning already configured

--- api/rest/router/router.go
+++ api/rest/router/router.go
@@ -1,8 +1,12 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"api/rest/routes"
+	"github.com/gorilla/mux"
+)
 
 // GenerateRouter returns a router with configured routes
 func GenerateRouter() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()          //created the router
+	return routes.Config_route(r) //returns the router created and configured
 }
