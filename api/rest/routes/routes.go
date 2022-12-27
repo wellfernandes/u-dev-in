ae745052f5b7c2118a40e48fd5403a33ad8bfac7
Welliton Fernandes Leal <wellfernandes@me.com>
2022-12-27 12:21:45 -0300

    added routes

--- api/rest/routes/routes.go
+++ api/rest/routes/routes.go
@@ -1,1 +1,28 @@
 package routes
+
+import (
+	"api/rest"
+	sentryGin "github.com/getsentry/sentry-go/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func Routes(s *rest.ServerRest) {
+
+	s.Engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	s.Engine.Use(sentryGin.New(sentryGin.Options{
+		WaitForDelivery: true,
+	}))
+
+	route1 := s.Engine.Group("/")
+	{
+		userGroup := route1.Group("user")
+		{
+			userGroup.POST("/", s.Controllers.UserController.Create)
+			userGroup.GET("/:id", s.Controllers.UserController.GetByID)
+			userGroup.PUT("/", s.Controllers.UserController.Update)
+			userGroup.DELETE("/:id", s.Controllers.UserController.Delete)
+		}
+	}
+}
