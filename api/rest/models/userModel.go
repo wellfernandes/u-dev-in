30b2d14a769a338026e96df58da5f7673e88cc03
Welliton Fernandes Leal <wellfernandes@me.com>
2022-12-27 10:25:24 -0300

    changed user attributes

--- api/rest/models/userModel.go
+++ api/rest/models/userModel.go
@@ -4,10 +4,10 @@ import "time"
 
 // User is the user model
 type User struct {
-	ID        uint64    `json:"id,omitempty"`
-	Name      string    `json:"name,omitempty"`
-	Nick      string    `json:"nick,omitempty"`
-	Email     string    `json:"email,omitempty"`
-	Password  string    `json:"password,omitempty"`
-	CreatedOn time.Time `json:"createdOn,omitempty"`
+	ID        uint64    `json:"id"`
+	Name      string    `json:"name"`
+	Nick      string    `json:"nick"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
 }
