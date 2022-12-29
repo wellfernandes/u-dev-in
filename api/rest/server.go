4500f0beadd63087ea0e370319623e3a11e8ab79
Welliton Fernandes Leal <wellfernandes@me.com>
2022-12-29 11:59:12 -0300

    added rest server

--- api/rest/server.go
+++ api/rest/server.go
@@ -7,7 +7,30 @@ import (
 )
 
 type ServerRest struct {
-	httpServer *http.Server
-	engine     *gin.Engine
-	config     *config.Config
+	HttpServer  *http.Server
+	Engine      *gin.Engine
+	Config      *config.Config
+	Controllers *Controllers
+}
+
+type userController interface {
+	Create(c *gin.Context)
+	GetByID(c *gin.Context)
+	Update(c *gin.Context)
+	Delete(c *gin.Context)
+}
+
+type Controllers struct {
+	UserController userController
+}
+
+func NewServerRest(config *config.Config, controllers *Controllers) *ServerRest {
+
+	engine := gin.Default()
+
+	return &ServerRest{
+		Engine:      engine,
+		Config:      config,
+		Controllers: controllers,
+	}
 }
