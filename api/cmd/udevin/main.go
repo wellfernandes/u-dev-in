74ebe77d510197f84611bc33818b84ff46eabe17
Welliton Fernandes Leal <wellfernandes@me.com>
2022-12-30 23:11:46 -0300

    added main

--- api/cmd/udevin/main.go
+++ api/cmd/udevin/main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"api/config"
-	"api/rest/router"
-	"fmt"
-	"log"
-	"net/http"
-)
-
 func main() {
 
-	// Configure the connection to the database.
-	config.LoadConfig()
-
-	// Create the router with the configured routes.
-	r := router.GenerateRouter()
-
-	fmt.Println("API is running!")
-
-	// Print the connection port to the terminal.
-	fmt.Println(fmt.Sprintf("Listening to port %d...", config.ConnectionPort))
-
-	// Starts the http server using the port from the config package.
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.ConnectionPort), r))
 }
